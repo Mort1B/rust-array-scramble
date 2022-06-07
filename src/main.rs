bd1cd787f49b0eac30ea0930080bbcb423c7371d
Morten Bergseng <bergseng.m@gmail.com>
2022-06-07 11:51:42 +0200

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,29 @@ fn main() {
 
     // 2D arrays using ndarray crate
     let arr4 = array![[1., 2., 3.], [4., 5., 6.]];
-    let arr5 = Array::from_elem((2, 1), 1.);
+    let arr5 = Array::from_elem((2, 1), 2.);
     let arr6 = arr4 + arr5;
     println!("2D array: \n{}", arr6);
+
+    // Fill arr6 with 0s
+    let arr7 = Array::<f64, _>::zeros(arr6.raw_dim());
+    let arr8 = arr6 * arr7;
+    println!("\n {}", arr8);
+
+    // identity matrix
+    let identity: &Array2<f64> = &Array::eye(3);
+    println!("\n identity matrix start: \n\n {}", identity);
+    let arr9 = array![[1., 2., 3.], [4., 5., 6.], [7., 8., 9.]];
+    let arr10 = &arr9 * identity;
+    println!("\n{}", arr10);
+    let arr11 = arr9.dot(identity);
+    println!("\n{}", arr11);
+
+    //0-D array, just an element
+    println!(" \n0-D array \n {}", array![2.]);
+    println!("Dimensions: {}", array![2.].ndim());
+
+    //3D or more array
+    let arr12 = Array::<i8, _>::ones((2, 3, 2, 2));
+    println!("\nMULTIDIMENSIONAL\n{}", arr12);
 }
